59a52390e66008e26a6e249a0a4041513da6227f
farooq <145667687+farucq@users.noreply.github.com>
2025-07-15 21:53:25 +0530

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,35 +1,79 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css'
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <div className='brandName'>
-        <img src='https://thumbs.dreamstime.com/b/airplane-logo-illustration-white-background-339249645.jpg'/>
-        <span>RouteX</span>
-      </div>
-      <div className='actions'>
-        <span className='home'>HOME▾</span>
-        <span className='about'>ABOUT US</span>
-        <span className='story'>STORY▾</span>
-        <span className='visa'>VISA▾</span>
-        <span className='blog'>BLOG▾ </span>
-        <span className='page'>PAGE▾</span>
-        <span className='contact'>CONTACT</span>
-      </div>
-      
-      <div className='help'>
-        <div className='helpImg'>
-          <img src='https://cdn-icons-png.flaticon.com/512/6072/6072846.png'/>
+    <nav className="navbar navbar-expand-lg bg-white fixed-top">
+      <div className="container-fluid">
+
+        {/* Brand */}
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img
+            src="https://thumbs.dreamstime.com/b/airplane-logo-illustration-white-background-339249645.jpg"
+            alt="logo"
+            width="40"
+            className="me-1"
+          />
+          <span className=" fw-bold text-brown">RouteX</span>
+        </a>
+
+        {/* Toggle button */}
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle= 'collapse'
+          data-bs-target= '#navbarNav'
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible content */}
+        <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+          
+          {/* Menu items */}
+          <ul className="navbar-nav mx-auto text-center">
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">HOME▾</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">ABOUT US</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">STORY▾</a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link mx-2" href="#">VISA▾</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">BLOG▾</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">PAGE▾</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-2" href="#">CONTACT</a>
+            </li>
+          </ul>
+
+          {/* Help section */}
+          <div className="helpSection d-flex">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/6072/6072846.png"
+              alt="help"
+              width="35"
+              className="me-2"
+            />
+            <div className="text-end">
+              <div className="text-muted small">Need help?</div>
+              <div>9072028649</div>
+            </div>
+          </div>
+
         </div>
-        <div className='helpContact'>
-          <span className='needHelp'>Need help?</span><br/>
-          <span >9072028649</span>
-        </div>
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
